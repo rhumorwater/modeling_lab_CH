040e13e9991de908216ea051693b4b1e1f5e38ba
razzywfg <romeo.bedague@epfl.ch>
2026-09-18 10:25:47 +0200

    first intitiation

---
+++ project_1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy
+
+
+doc = pd.read_csv("periodic_table.csv", header=0)
+# doc = open("C:/Users/rombe/Desktop/BA5/modeling_lab/periodic_table.csv",'r')  
+
+
+print(doc)
+
+
